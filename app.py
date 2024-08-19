73984622684b67f22d34428adc4d95cac4f03525
amrutadat <119044469+amrutadat@users.noreply.github.com>
2024-08-19 16:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def input_pdf_setup(uploaded_file):
 ## Streamlit App
 
 st.set_page_config(page_title="ATS Resume EXpert")
-st.header("ATS Tracking System")
+st.header("Application Match System")
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your first resume(PDF)...",type=["pdf"])
 uploaded_file1=st.file_uploader("Upload your second resume(PDF)...",type=["pdf"])
-uploaded_file2=st.file_uploader("Upload your third resume(PDF)...",accept_multiple_files=True,type=["pdf"])
+uploaded_file2=st.file_uploader("Upload your third resume(PDF)...",type=["pdf"])
 
 if uploaded_file is not None:
     st.write("PDF Uploaded Successfully")
@@ -84,7 +84,7 @@ if submit3:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
         response=get_gemini_response(input_prompt3,pdf_content,input_text)
-        st.subheader("The Repsonse for uploaded resumes")
+        st.subheader("The Repsonse for first uploaded resume")
         st.write(response)
     else:
         st.write("Please uplaod the resume")
